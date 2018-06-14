9362a0ca7c13c6a6c8570fb710fad80e325f7ec7
Kevin Choong <kmaehya@gmail.com>
2018-06-14 19:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if (string.toLowerCase() === string) {
+    console.log("I can't hear you!");
+  } else if (string.toUpperCase() === string) {
+    console.log("YES INDEED!");
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
